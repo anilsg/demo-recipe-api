12b635988b8fd8e0394b429ed76a52fed85fa6ff
anilsg <anilsg@users.noreply.github.com>
2019-03-12 22:49:48 +1100

    Added tests for logging calls to RouterTest

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -14,9 +14,13 @@ class RouterTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
     */
     public function testGetIngredients()
     {
+        $logger = \Mockery::mock('recipe\app\LoggerProvider'); // Mock the LoggerProvider.
+        $logger->shouldReceive('addWarning')->once()->andReturnNull(); // Expect a single call to log the GET request.
+
         $json = '[{"title":"Ham","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Cheese","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Bread","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Butter","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Bacon","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Eggs","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Mushrooms","best-before":"2019-02-22","use-by":"2019-02-25"},{"title":"Sausage","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Hotdog Bun","best-before":"2019-02-22","use-by":"2019-03-09"},{"title":"Ketchup","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Mustard","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Lettuce","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Tomato","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Cucumber","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Beetroot","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Salad Dressing","best-before":"2019-02-22","use-by":"2019-02-25"}]';
         $ingredients = json_decode($json); // Artificially create the test data, not testing DataProvider.
-        $container = ['data'=>['ingredients'=>$ingredients]]; // Canonical location for data access.
+
+        $container = ['data'=>['ingredients'=>$ingredients], 'logger'=>$logger ]; // Configure data and logger.
         $container = new \Slim\Container($container); // Converts array to \Slim\Container.
         $router = new \recipe\app\Router($container); // Not testing \Slim\App testing \recipe\app\Router.
         $environment = \Slim\Http\Environment::mock([ // Mock a GET /ingredients request.
@@ -35,9 +39,13 @@ class RouterTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
     */
     public function testGetRecipes()
     {
+        $logger = \Mockery::mock('recipe\app\LoggerProvider'); // Mock the LoggerProvider.
+        $logger->shouldReceive('addWarning')->once()->andReturnNull(); // Expect a single call to log the GET request.
+
         $json = '[{"title":"Ham and Cheese Toastie","ingredients":["Ham","Cheese","Bread","Butter"]},{"title":"Fry-up","ingredients":["Bacon","Eggs","Baked Beans","Mushrooms","Sausage","Bread"]},{"title":"Salad","ingredients":["Lettuce","Tomato","Cucumber","Beetroot","Salad Dressing"]},{"title":"Hotdog","ingredients":["Hotdog Bun","Sausage","Ketchup","Mustard"]},{"title":"Omelette","ingredients":["Eggs","Mushrooms","Milk","Salt","Pepper","Spinach"]}]';
         $recipes = json_decode($json); // Artificially create the test data, not testing DataProvider.
-        $container = ['data'=>['recipes'=>$recipes]]; // Canonical location for data access.
+
+        $container = ['data'=>['recipes'=>$recipes], 'logger'=>$logger ]; // Configure data and logger.
         $container = new \Slim\Container($container); // Includes array in new \Slim\Container.
         $router = new \recipe\app\Router($container); // Not testing \Slim\App testing \recipe\app\Router.
         $environment = \Slim\Http\Environment::mock([ // Mock a GET /recipes request.
@@ -56,7 +64,11 @@ class RouterTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
     */
     public function testGetLunch()
     {
-        $container = new \Slim\Container(['data'=>['ingredients'=>[], 'recipes'=>[]]]);
+        $logger = \Mockery::mock('recipe\app\LoggerProvider'); // Mock the LoggerProvider.
+        $logger->shouldReceive('addWarning')->once()->andReturnNull(); // Expect a single call to log the GET request.
+
+        $container = ['data'=>['ingredients'=>[], 'recipes'=>[]], 'logger'=>$logger]; // Configure minimal data and logger.
+        $container = new \Slim\Container($container);
         $router = new \recipe\app\Router($container); // Not testing \Slim\App testing \recipe\app\Router.
         $environment = \Slim\Http\Environment::mock([ // Mock a GET /recipes request.
             'REQUEST_METHOD' => 'GET',

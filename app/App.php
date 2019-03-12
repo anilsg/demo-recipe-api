dfad59df6cebe785dbdce811111156c5d1e0f419
anilsg <anilsg@users.noreply.github.com>
2019-03-12 16:24:44 +1100

    Added basic API endpoints for ingredients recipes lunch.

--- app/App.php
+++ app/App.php
@@ -5,19 +5,70 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Container\ContainerInterface;
 
+/**
+* Implements the REST API endpoints.
+* Loads data, performs logging and calls other objects providing core functionality.
+* @package    recipe
+* @subpackage app
+* @version    $Id:$
+* @author     anilsg
+* @link       https://github.com/anilsg/demo-recipe-api
+*/
 class App
 {
     protected $container;
 
+    /**
+    * Constructor required to pass through access to the dependency injection (DI) container.
+    * Arbitrary properties of the container contain settings and references to data and services.
+    * @param \Psr\Container\ContainerInterface $container  Slim\App DI container
+    */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
-    public function __invoke(Request $request, Response $response, $args = [])
+    /**
+    * Simply provide the current ingredients list as JSON.
+    * @param \Psr\Http\Message\ServerRequestInterface
+    * @param \Psr\Http\Message\ResponseInterface
+    * @param array
+    * @return \Psr\Http\Message\ResponseInterface
+    */
+    public function getIngredients(Request $request, Response $response, $args = [])
     {
-        $name = $args['name'];
-        $response->getBody()->write("Hello, $name<br>");
-        return $response;
+        // construction in progress
+        $json = '{"ingredients":[{"title":"Ham","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Cheese","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Bread","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Butter","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Bacon","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Eggs","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Mushrooms","best-before":"2019-02-22","use-by":"2019-02-25"},{"title":"Sausage","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Hotdog Bun","best-before":"2019-02-22","use-by":"2019-03-09"},{"title":"Ketchup","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Mustard","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Lettuce","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Tomato","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Cucumber","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Beetroot","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Salad Dressing","best-before":"2019-02-22","use-by":"2019-02-25"}]}';
+        $ingredients = json_decode($json);
+        return $response->withJson($ingredients);
     }
+
+    /**
+    * Simply provide the current recipes list as JSON.
+    * @param \Psr\Http\Message\ServerRequestInterface
+    * @param \Psr\Http\Message\ResponseInterface
+    * @param array
+    * @return \Psr\Http\Message\ResponseInterface
+    */
+    public function getRecipes(Request $request, Response $response, $args = [])
+    {
+        // construction in progress
+        $json = '{"recipes":[{"title":"Ham and Cheese Toastie","ingredients":["Ham","Cheese","Bread","Butter"]},{"title":"Fry-up","ingredients":["Bacon","Eggs","Baked Beans","Mushrooms","Sausage","Bread"]},{"title":"Salad","ingredients":["Lettuce","Tomato","Cucumber","Beetroot","Salad Dressing"]},{"title":"Hotdog","ingredients":["Hotdog Bun","Sausage","Ketchup","Mustard"]},{"title":"Omelette","ingredients":["Eggs","Mushrooms","Milk","Salt","Pepper","Spinach"]}]}';
+        $recipes = json_decode($json);
+        return $response->withJson($recipes);
+    }
+
+    /**
+    * API endpoint to call recipe\app\Lunch and return the specified lunch recipes JSON.
+    * @param \Psr\Http\Message\ServerRequestInterface
+    * @param \Psr\Http\Message\ResponseInterface
+    * @param array
+    * @return \Psr\Http\Message\ResponseInterface
+    */
+    public function getLunch(Request $request, Response $response, $args = [])
+    {
+        // construction in progress
+        return $response->getBody()->write('<h1 style="color:red">Lunch Time!</h1>');
+    }
+
 }
--- pub/index.php
+++ pub/index.php
@@ -3,11 +3,29 @@ namespace recipe\pub;
 
 require '../vendor/autoload.php';
 
+/**
+* Entry point to start the recipe REST API app.
+* Dependencies and settings are configured here and loaded into the DI container.
+* Further processing and instantiation occurs in the invoked classes.
+* @package    recipe
+* @subpackage pub
+* @version    $Id:$
+* @author     anilsg
+* @link       https://github.com/anilsg/demo-recipe-api
+*/
+
+// Configure SlimPHP app settings in the DI container.
+// Settings like displayErrorDetails are suitable for dev only.
+// Enhance this by loading settings from the environment,
+// to support transition to other settings in stage and prod.
 $settings['displayErrorDetails'] = true;
 $settings['addContentLengthHeader'] = false;
 $app = new \Slim\App(['settings' => $settings]);
 
-$app->get('/hello/{name}', \recipe\app\App::class);
+// Create Slim\App URI endpoints for REST API.
+$app->get('/ingredients', \recipe\app\App::class . ':getIngredients');
+$app->get('/recipes', \recipe\app\App::class . ':getRecipes');
+$app->get('/lunch', \recipe\app\App::class . ':getLunch');
 
 $app->run();
 

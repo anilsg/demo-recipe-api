25af0d6c0ecf0ea4e40c0d0abf3d341e4ef4c2cf
anilsg <anilsg@users.noreply.github.com>
2019-03-12 22:51:47 +1100

    Corrected RouterTest logging tests to predict correct method addInfo not addWarn

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -15,7 +15,7 @@ class RouterTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
     public function testGetIngredients()
     {
         $logger = \Mockery::mock('recipe\app\LoggerProvider'); // Mock the LoggerProvider.
-        $logger->shouldReceive('addWarning')->once()->andReturnNull(); // Expect a single call to log the GET request.
+        $logger->shouldReceive('addInfo')->once()->andReturnNull(); // Expect a single call to log the GET request.
 
         $json = '[{"title":"Ham","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Cheese","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Bread","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Butter","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Bacon","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Eggs","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Mushrooms","best-before":"2019-02-22","use-by":"2019-02-25"},{"title":"Sausage","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Hotdog Bun","best-before":"2019-02-22","use-by":"2019-03-09"},{"title":"Ketchup","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Mustard","best-before":"2019-03-09","use-by":"2019-03-14"},{"title":"Lettuce","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Tomato","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Cucumber","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Beetroot","best-before":"2019-03-04","use-by":"2019-03-09"},{"title":"Salad Dressing","best-before":"2019-02-22","use-by":"2019-02-25"}]';
         $ingredients = json_decode($json); // Artificially create the test data, not testing DataProvider.
@@ -40,7 +40,7 @@ class RouterTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
     public function testGetRecipes()
     {
         $logger = \Mockery::mock('recipe\app\LoggerProvider'); // Mock the LoggerProvider.
-        $logger->shouldReceive('addWarning')->once()->andReturnNull(); // Expect a single call to log the GET request.
+        $logger->shouldReceive('addInfo')->once()->andReturnNull(); // Expect a single call to log the GET request.
 
         $json = '[{"title":"Ham and Cheese Toastie","ingredients":["Ham","Cheese","Bread","Butter"]},{"title":"Fry-up","ingredients":["Bacon","Eggs","Baked Beans","Mushrooms","Sausage","Bread"]},{"title":"Salad","ingredients":["Lettuce","Tomato","Cucumber","Beetroot","Salad Dressing"]},{"title":"Hotdog","ingredients":["Hotdog Bun","Sausage","Ketchup","Mustard"]},{"title":"Omelette","ingredients":["Eggs","Mushrooms","Milk","Salt","Pepper","Spinach"]}]';
         $recipes = json_decode($json); // Artificially create the test data, not testing DataProvider.
@@ -65,7 +65,7 @@ class RouterTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
     public function testGetLunch()
     {
         $logger = \Mockery::mock('recipe\app\LoggerProvider'); // Mock the LoggerProvider.
-        $logger->shouldReceive('addWarning')->once()->andReturnNull(); // Expect a single call to log the GET request.
+        $logger->shouldReceive('addInfo')->once()->andReturnNull(); // Expect a single call to log the GET request.
 
         $container = ['data'=>['ingredients'=>[], 'recipes'=>[]], 'logger'=>$logger]; // Configure minimal data and logger.
         $container = new \Slim\Container($container);
